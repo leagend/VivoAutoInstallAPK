cadd4336863ff597088d66ee4f9f6e9f82f97b01
Min Deng <minotes.net@gmail.com>
2019-05-25 14:50:06 +0800

    add foreground service

--- app/src/main/java/com/jianwu/vivoautoinstallapk/MainActivity.java
+++ app/src/main/java/com/jianwu/vivoautoinstallapk/MainActivity.java
@@ -1,11 +1,18 @@
 package com.jianwu.vivoautoinstallapk;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,6 +37,47 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        updateServiceStatus();
+        startService();
+    }
+
+    private void updateServiceStatus() {
+        TextView textView = findViewById((R.id.textView));
+        textView.setText(isAccessibilitySettingsOn(getApplicationContext()) ? "服务已开启" : "服务未开启");
+    }
+
+    private void startService() {
+        Intent serviceIntent = new Intent();
+        serviceIntent.setClass(getApplicationContext(), MyService.class);
+
+        ComponentName result = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            result = startForegroundService(serviceIntent);
+        } else {
+            result = startService(serviceIntent);
+        }
+
+        if(result != null) {
+            Toast.makeText(getBaseContext(), "Service is already running", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getBaseContext(), "There is no service running, starting service..", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void savePwd() {
@@ -40,5 +88,30 @@ public class MainActivity extends AppCompatActivity {
 
         SharePreferencesUtils.setParam(getApplication(), AppConstants.KEY_PASSWORD, pwdStr);
         Toast.makeText(getApplication(), "保存成功", Toast.LENGTH_SHORT).show();
+    }
+
+    public boolean isAccessibilitySettingsOn(Context mContext) {
+        int accessibilityEnabled = 0;
+        final String service = getPackageName() + "/" + InstallerHelperService.class.getCanonicalName();
+        try {
+            accessibilityEnabled = Settings.Secure.getInt(mContext.getApplicationContext().getContentResolver(),
+                    Settings.Secure.ACCESSIBILITY_ENABLED);
+        } catch (Settings.SettingNotFoundException ignored) {
+        }
+        TextUtils.SimpleStringSplitter mStringColonSplitter = new TextUtils.SimpleStringSplitter(':');
+        if (accessibilityEnabled == 1) {
+            String settingValue = Settings.Secure.getString(mContext.getApplicationContext().getContentResolver(),
+                    Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+            if (settingValue != null) {
+                mStringColonSplitter.setString(settingValue);
+                while (mStringColonSplitter.hasNext()) {
+                    String accessibilityService = mStringColonSplitter.next();
+                    if (accessibilityService.equalsIgnoreCase(service)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
